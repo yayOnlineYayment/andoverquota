92c7bf6f302d586f428b6a4ad2aee6aa6e8cf261
michaelmessina <m2messina@k12.andoverma.us>
2017-12-20 14:17:37 -0500

    Updated News section
    Removed Idol ad and replaced with photo gallery.

--- js/init.js
+++ js/init.js
@@ -2,8 +2,7 @@ $(document).ready(function () {
     $('.materialboxed').materialbox();
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
-   // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+    // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
     $('.modal').modal();
-   $('.collapsible').collapsible();
-    $('.materialboxed').materialbox();
+    $('.collapsible').collapsible();
 });
